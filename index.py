94808307486c2e87a577912935cadd8bf670ec4b
Cuitewhitney <118156156+Cuitewhitney@users.noreply.github.com>
2025-03-01 19:02:38 +0100

    My second commit

--- index.py
+++ index.py
@@ -3,3 +3,14 @@ def Sum_of_two_numbers(a,b):
     return a + b
 
 print(Sum_of_two_numbers(23,21))
+print(Sum_of_two_numbers(90,81))
+
+
+# pyhton code to return the product of two number
+
+def product(x,y):
+    return x * y
+
+
+print(product(2,8))
+    
